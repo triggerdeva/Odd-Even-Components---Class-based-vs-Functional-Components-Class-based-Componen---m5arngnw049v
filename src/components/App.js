7cb1223777ef47661c1a127b282e3cbf12c8efc4
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-03-27 19:27:43 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,42 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
 
-class App extends React.Component{
-  
-  handleClick(){
-    if(this.state.flag){
-      this.setState({count: this.state.count + 2,flag:false});
-    }
-    else this.setState({flag:true});
+class Odd extends React.Component {
+  componentWillUnmount() {
+    console.log("Odd is unmounted");
   }
-  constructor(props){
-    super(props)
-    this.state = {count:0,flag:false}
-    this.handleClick = this.handleClick.bind(this)
+  render() {
+    return <div id="odd">I am odd</div>;
   }
+}
 
+class Even extends React.Component {
+  componentWillUnmount() {
+    console.log("Even is unmounted");
+  }
+  render() {
+    return <div id="even">I am even</div>;
+  }
+}
+class App extends React.Component {
+  handleChange() {
+    this.setState({ even: !this.state.even });
+  }
+  constructor(props) {
+    super(props);
+    this.state = { even: true };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  render() {
+    return (
+      <div id="main">
+        {this.state.even ? <Even /> : <Odd />}
 
-  render(){
-    {!this.state.flag && console.log(`Rendering with count:-${this.state.count}`)};
-    return(
-      <div>
-        <span id="count">{this.state.count}</span>
-        <button id="incr-btn" onClick={this.handleClick}>Increment</button>
+        <button id="toggle" onClick={this.handleChange}>
+          Change
+        </button>
       </div>
-    )
-
+    );
   }
 }
 
